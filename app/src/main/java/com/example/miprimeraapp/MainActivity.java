78df047856863894bcaf538643e9662ebae810f3
Jaime Salmeron <expresscliente138@gmail.com>
2026-05-14 22:49:56 -0600

    Creacion de la rama mensajeria, configuracion y sincronizacion con el proyecto de firebase y android studio, y se realizaron permisos en android para la app de mesanjeria

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -1,55 +1,30 @@
 package com.example.miprimeraapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends Activity {
+    FloatingActionButton fab;
     Button btn;
-    Spinner spn;
+    TextView tempVal;
+    String accion="", idAmigo="", id="", rev="";
+    ImageView img;
+    String urlCompletaFoto="", getUrlCompletaFotoFireStore="";
+    Intent tomarFotoIntent;
+    detectarinternet di;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v->calcular());
-    }
-    private void calcular(){
-        tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
-
-        tempVal = findViewById(R.id.txtNum2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 0;
-
-        spn = findViewById(R.id.cboOpciones);
-        switch (spn.getSelectedItemPosition()){
-            case 0: //suma
-                respuesta = num1 + num2;
-                break;
-            case 1: //Resta
-                respuesta = num1 - num2;
-                break;
-            case 2: //Multiplicacion
-                respuesta = num1 * num2;
-                break;
-            case 3: //division
-                respuesta = num1 / num2;
-                break;
-        }
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
     }
 }
